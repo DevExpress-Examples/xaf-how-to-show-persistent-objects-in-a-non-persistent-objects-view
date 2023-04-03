eafc984cfebdde0a5bb5acf49dee4aa9f42c7f1f
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-03 20:26:39 +0400

    xpo version

---
+++ CS/XPO/ComplexDialog/ComplexDialog.Module/Controllers/MyController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Editors;
+using ComplexDialogSample.Module.BusinessObjects;
+
+namespace ComplexDialogSample.Module.Controllers {
+    public class MyController : ViewController {
+        public MyController() {
+            TargetObjectType = typeof(Office);
+            TargetViewType = ViewType.ListView;
+            PopupWindowShowAction action = new PopupWindowShowAction(this, "AssignJobs", PredefinedCategory.RecordEdit);
+            action.SelectionDependencyType = SelectionDependencyType.RequireMultipleObjects;
+            action.CustomizePopupWindowParams += new CustomizePopupWindowParamsEventHandler(action_CustomizePopupWindowParams);
+            action.Execute += new PopupWindowShowActionExecuteEventHandler(action_Execute);
+        }
+        void action_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e) {
+            IObjectSpace os = Application.CreateObjectSpace(typeof(OrderTemplate));
+            e.Context = TemplateContext.PopupWindow;
+            var template = os.CreateObject<OrderTemplate>();
+            e.View = Application.CreateDetailView(os, template);
+        }
+        void action_Execute(object sender, PopupWindowShowActionExecuteEventArgs e) {
+            OrderTemplate parameters =(OrderTemplate) e.PopupWindow.View.CurrentObject;
+            ListPropertyEditor listPropertyEditor = ((DetailView)e.PopupWindow.View).FindItem("Services") as ListPropertyEditor;
+            IObjectSpace os = Application.CreateObjectSpace(typeof(Team));
+            foreach (Office b in e.SelectedObjects) {
+                Team team = os.GetObject<Team>(parameters.Team);
+                foreach (Service service in listPropertyEditor.ListView.SelectedObjects) {
+                    Order order = os.CreateObject<Order>();
+                    order.DueDate = parameters.DueDate;
+                    order.Team = team;
+                    order.Office = os.GetObject<Office>(b);
+                    order.Service = os.GetObject<Service>(service);
+                }
+            }
+            os.CommitChanges();
+        }
+    }
+}
---
+++ CS/XPO/ComplexDialog/ComplexDialog.Module/Module.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+
+namespace ComplexDialog.Module;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
+public sealed class ComplexDialogModule : ModuleBase {
+    public ComplexDialogModule() {
+		// 
+		// ComplexDialogModule
+		// 
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
+    }
+    public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        return new ModuleUpdater[] { updater };
+    }
+    public override void Setup(XafApplication application) {
+        base.Setup(application);
+        application.ObjectSpaceCreated += Application_ObjectSpaceCreated;
+        // Manage various aspects of the application UI and behavior at the module level.
+    }
+    private void Application_ObjectSpaceCreated(object sender, ObjectSpaceCreatedEventArgs e) {
+        CompositeObjectSpace compositeObjectSpace = e.ObjectSpace as CompositeObjectSpace;
+        if (compositeObjectSpace != null) {
+            if (!(compositeObjectSpace.Owner is CompositeObjectSpace)) {
+                compositeObjectSpace.PopulateAdditionalObjectSpaces((XafApplication)sender);
+            }
+        }
+    }
+    public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
+        base.CustomizeTypesInfo(typesInfo);
+        CalculatedPersistentAliasHelper.CustomizeTypesInfo(typesInfo);
+    }
+}
