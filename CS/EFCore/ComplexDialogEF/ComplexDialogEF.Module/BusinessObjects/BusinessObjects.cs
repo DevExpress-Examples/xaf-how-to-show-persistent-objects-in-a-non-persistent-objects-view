579f126e2f05570145eb9afd7e8367fc1f2f6a64
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-03 20:34:42 +0400

    ef

---
+++ CS/EFCore/ComplexDialogEF/ComplexDialogEF.Module/BusinessObjects/BusinessObjects.cs
@@ -0,0 +1,60 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Xpo;
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+
+namespace ComplexDialogSample.Module.BusinessObjects {
+    [DomainComponent]
+    public class OrderTemplate : IObjectSpaceLink {
+        public DateTime DueDate { get; set; }
+        public Team Team { get; set; }
+        private IList<Service> _services;
+        public IList<Service> Services {
+            get {
+                if (_services == null) {
+                    _services = ObjectSpace.GetObjects<Service>();
+                }
+                return _services;
+            }
+        }
+        private IObjectSpace objectSpace;
+        [Browsable(false)]
+        public IObjectSpace ObjectSpace {
+            get { return objectSpace; }
+            set { objectSpace = value; }
+        }
+    }
+
+
+    [NavigationItem("Resources")]
+    public class Team : BaseObject {
+        public virtual string Name { get; set; }
+    }
+
+    [NavigationItem("Process")]
+    [DefaultProperty(nameof(Index))]
+    public class Office : BaseObject {
+        public virtual string Index { get; set; }
+        public virtual string OccupiedBy { get; set; }
+    }
+
+    [NavigationItem("Process")]
+    public class Order : BaseObject {
+        public virtual Team Team { get; set; }
+        public virtual Office Office { get; set; }
+        public virtual Service Service { get; set; }
+        public virtual DateTime DueDate { get; set; }
+    }
+
+    [NavigationItem("Resources")]
+    [DefaultProperty(nameof(Description))]
+    public class Service : BaseObject {
+        public virtual string Description { get; set; }
+    }
+}
---
+++ CS/EFCore/ComplexDialogEF/ComplexDialogEF.Module/Module.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+
+namespace ComplexDialogEF.Module;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
+public sealed class ComplexDialogEFModule : ModuleBase {
+    public ComplexDialogEFModule() {
+		// 
+		// ComplexDialogEFModule
+		// 
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
+    }
+    public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        return new ModuleUpdater[] { updater };
+    }
+    public override void Setup(XafApplication application) {
+        base.Setup(application);
+        application.ObjectSpaceCreated += Application_ObjectSpaceCreated;
+        // Manage various aspects of the application UI and behavior at the module level.
+    }
+    private void Application_ObjectSpaceCreated(object sender, ObjectSpaceCreatedEventArgs e) {
+        CompositeObjectSpace compositeObjectSpace = e.ObjectSpace as CompositeObjectSpace;
+        if (compositeObjectSpace != null) {
+            if (!(compositeObjectSpace.Owner is CompositeObjectSpace)) {
+                compositeObjectSpace.PopulateAdditionalObjectSpaces((XafApplication)sender);
+            }
+        }
+    }
+}
